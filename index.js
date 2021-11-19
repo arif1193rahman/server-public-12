9a0d2f94268e081b5c4725864888a2063605954d
Md Arifur Rahman <rahman1193arif@gmail.com>
2021-11-19 22:43:03 +0600

    finish

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l9eit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 //   console.log('coooool',uri);
 
-
-
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,120 +24,81 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("secureProperty").collection("services");
-    // const bookingCollect = client.db("secureProperty").collection("booking");
-     const exploreCollect = client.db("secureProperty");
-     const exploreServiceConnection= exploreCollect.collection("explore");
-    // const usersConnection = database.collection('users'); 
+    const exploreCollect = client.db("secureProperty");
+    const exploreServiceConnection = exploreCollect.collection("explore");
+    const review = client.db("secureProperty");
+    const reviewConnection = review.collection("reviewPart");
 
-    // console.log('connect to database');
+    //  Get API
+    app.get("/services", async (req, res) => {
+      const cursor = database.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-//  Get API
- app.get("/services", async (req, res) => {
-  const cursor = database.find({});
-  const services = await cursor.toArray();
-  res.send(services);
-});
-
-// Get single service
-app.get('/services/:id', async(req, res)=>{
-  const id = req.params.id;
-  // console.log('getting specifi service', id)
-  const query = {_id: ObjectId(id)};
-  const services = await database.findOne(query);
-  res.json(services);
-})
+    // Get single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const services = await database.findOne(query);
+      res.json(services);
+    });
 
     // POST API
-    app.post('/services',async(req, res)=>{
-        const service = req.body;
-        // console.log(service);
-        const result = await database.insertOne(service);
-        // console.log('hit the post API');
-        res.json(result)
-    } )
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await database.insertOne(service);
+      res.json(result);
+    });
 
-// explore Collection
+    //get explore Collection
 
-app.get('/explore', async(req, res)=>{
-  const cursor = exploreServiceConnection.find({});
-  const services = await cursor.toArray();
-  res.send(services);
-})
+    app.get("/explore", async (req, res) => {
+      const cursor = exploreServiceConnection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-// app.post('/explore', async(req, res)=>{
-//   const explore = {
-//     "img": "https://i.ibb.co/jTXb9Sg/wireless-Camera1-400x250.jpg",
-//     "brand": "Wireless CCTV",
-//     "overview": "Zxtech 5MP Wireless CCTV System - 1x WiFi Security Camera Outdoor 2-Way-Audio Night Vision 9CH Sony Starvis | WF1A9Y",
-//     "range": "80 FIT",
-//      "price": 16000
-//   }
-//   const result = await exploreServiceConnection.insertOne(explore);
-//   console.log(result);
-// })
+    //post review reviewPart
+    app.post("/reviewPart", async (req, res) => {
+      const reviews = req.body;
+      const result = await reviewConnection.insertOne(reviews);
+      console.log(result);
+      res.json(result);
+    });
 
+    // Get all reviews
+    app.get("/reviewPart", async (req, res) => {
+      const cursor = reviewConnection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
 
+    app.put("/services/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await database.updateDoc(filter, updateDoc);
+      res.json(result);
+    });
 
-// User Collection
-// app.post('/users', async(req, res)=>{
-//   const user = req.body;
-//   const result = await usersConnection.insertOne(user);
-//   console.log(result);
-//   res.json(result);
-// })
-
-app.put('/services/admin', async(req, res)=>{
-  const user = req.body;
-  const filter = {email: user.email};
-  const updateDoc = {$set: {role: 'admin'}}
-  const result = await database.updateDoc(filter, updateDoc);
-  res.json(result);
-})
-
-    // Explore
-    // app.get("/explore", async (req, res) => {
-    //   const cursor = database.find({});
-    //   const explore = await cursor.toArray();
-    //   res.send(explore);
-    // });
-    
-     // Get single service
-    // app.get('/explore/:id', async(req, res)=>{
-    //   const id = req.params.id;
-       // console.log('getting specifi service', id)
-    //   const query = {_id: ObjectId(id)};
-    //   const explore = await database.findOne(query);
-    //   res.json(explore);
-    // })
-    
-        // POST API explore
-    // app.post('/explore',async(req, res)=>{
-    //         const explore = req.body;
-    //         const result = await exploreCollect.insertOne(explore);
-            // console.log('hit the post API');
-    //         res.json(result)
-    //     } )
-
-
-app.get('/services', async(req, res)=>{
-  const cursor = database.find({});
+    app.get("/services", async (req, res) => {
+      const cursor = database.find({});
       const booking = await cursor.toArray();
       res.send(booking);
-})
+    });
 
     // confirm order
-    app.post('/services', async(req, res)=>{
+    app.post("/services", async (req, res) => {
       const result = await database.insertOne(req.body);
-    res.json(result);
-    })
-
+      res.json(result);
+    });
 
     // My order
-    app.get('/myOrders/:email', async(req, res)=>{
-      const result = await database.find({email: req.params.email}).toArray();
+    app.get("/myOrders/:email", async (req, res) => {
+      const result = await database.find({ email: req.params.email }).toArray();
       res.json(result);
-    })
-
+    });
   } finally {
     // await clint.close();
   }
